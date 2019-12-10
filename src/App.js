d8cfc64735836b57689806fd0addab5f402b7d76
Issac Moreno <issaxavier909@gmail.com>
2019-12-10 15:36:20 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Display from "./components/DisplayComponents/Display";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators"
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -13,10 +19,26 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const [display, setDisplay] = useState('');
+  const addNumber = (number) => {
+    setDisplay(display + number);
+  }
+  const displayOperator = (operator) => {
+    if(operator === '=') {
+      setDisplay(display => eval(display))
+    } else {
+      setDisplay(display => `${display} ${operator} `);
+    }
+  }
+
   return (
     <div className="container">
-      <Logo />
       <div className="App">
+        <Logo />
+        <Display number = {display}/>
+        <Numbers addNumber = {addNumber}/>
+        <Operators displayOperator = {displayOperator}/>
+        <Specials />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
